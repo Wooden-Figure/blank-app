3db252a818603784cbf995f173e308f619ae2fbf
Robert Soto <148888873+Wooden-Figure@users.noreply.github.com>
2024-07-27 16:09:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,6 @@ st.write("This is the CVG 110 labor plan. Look at the images below to find your 
 # List of Google Drive direct download links for the images
 image_urls = [
     "https://drive.google.com/uc?export=download&id=1H-DsYQ7wgtUkyGY72u3oFrQV0Jlr_IHu",
-    '''"https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_2",  # Replace with actual file ID
-    "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_3",  # Replace with actual file ID
-    "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_4",  # Replace with actual file ID
-    "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_5",  # Replace with actual file ID
-    "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_6",  # Replace with actual file ID
-    "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_7",  # Replace with actual file ID
-    "https://drive.google.com/uc?export=download&id=YOUR_FILE_ID_8"   # Replace with actual file ID'''
 ]
 
 # Display images from Google Drive links
